4c1dccc0ba2794348709b2d062263e543e7ee281
Toru Maesaka <toru@torumk.com>
2020-05-19 19:06:35 -0700

    cmd/beamd: somehow missed pruning the main file

--- cmd/beamd/main.go
+++ cmd/beamd/main.go
@@ -2,24 +2,10 @@
 package main
 
 import (
-	"flag"
 	"log"
 
 	"github.com/toru/beam/store"
 )
-
-type config struct {
-	Store string
-	Port  int
-}
-
-func buildConfig() config {
-	var cfg config
-	flag.StringVar(&cfg.Store, "store", "memory", "storage engine name")
-	flag.IntVar(&cfg.Port, "port", 8080, "port number to listen on")
-	flag.Parse()
-	return cfg
-}
 
 func main() {
 	cfg := buildConfig()
