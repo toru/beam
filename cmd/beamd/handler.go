1e5006e3c273b72b2b31ebef8f1078fb7ac4515f
Toru Maesaka <toru@torumk.com>
2020-05-29 20:55:32 -0700

    cmd/beamd: add a util function for 404

--- cmd/beamd/handler.go
+++ cmd/beamd/handler.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/toru/beam/pkg/store"
 )
 
+func render404(w http.ResponseWriter) {
+	http.Error(w, strconv.Quote("not found"), http.StatusNotFound)
+}
+
 func bookmarksResourceHandlerFunc(db store.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "unimplemented")
+		switch r.Method {
+		case http.MethodPost:
+			fmt.Fprintf(w, strconv.Quote("unimplemented"))
+		default:
+			render404(w)
+		}
 	}
 }
 
