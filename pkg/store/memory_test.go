058b779c0b1ac3ecc03949d3867a4b6532855a49
Toru Maesaka <toru@torumk.com>
2020-06-19 19:28:53 -0700

    store/memory: update the test

--- pkg/store/memory_test.go
+++ pkg/store/memory_test.go
@@ -22,8 +22,8 @@ func TestBookmarks(t *testing.T) {
 	i1.SetURL("https://torumk.com/")
 	i2 := bookmark.NewItem()
 	i2.SetURL("https://ep.torumk.com/")
-	db.WriteBookmark(*i1)
-	db.WriteBookmark(*i2)
+	db.WriteBookmark(i1)
+	db.WriteBookmark(i2)
 
 	// Hack until we implement comparators.
 	keys := make([]string, 0, 2)
@@ -47,8 +47,8 @@ func TestBookmarkCount(t *testing.T) {
 	i1.SetURL("https://torumk.com/")
 	i2 := bookmark.NewItem()
 	i2.SetURL("https://ep.torumk.com/")
-	db.WriteBookmark(*i1)
-	db.WriteBookmark(*i2)
+	db.WriteBookmark(i1)
+	db.WriteBookmark(i2)
 	got := db.BookmarkCount()
 	want := 2
 	if got != want {
@@ -60,7 +60,7 @@ func TestWriteBookmark(t *testing.T) {
 	db := NewMemoryStore()
 	item := bookmark.NewItem()
 	item.SetURL("https://torumk.com/")
-	if err := db.WriteBookmark(*item); err != nil {
+	if err := db.WriteBookmark(item); err != nil {
 		t.Error(err)
 	}
 }
