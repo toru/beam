88d3cbaad810e7fb5864d6be1f434a06d4672c85
Toru Maesaka <toru@torumk.com>
2020-06-25 18:39:20 -0700

    auth/key: implement a constructor and duplicator

--- pkg/auth/key.go
+++ pkg/auth/key.go
@@ -1,6 +1,10 @@
 // Package auth provides code for API authentication and authorization.
 package auth
 
+import (
+	"time"
+)
+
 // Key represents an API key.
 type Key struct {
 	Name       string    // Name of the key
@@ -8,4 +12,17 @@ type Key struct {
 	LastUsedAt time.Time // When the key was last used
 	CreatedAt  time.Time // When the key was created
 	UpdatedAt  time.Time // When the key was last updated
+}
+
+// NewKey returns a pointer to a new Key.
+func NewKey() *Key {
+	return &Key{}
+}
+
+// Dup returns a copy of the given Key pointer value.
+func (key *Key) Dup() Item {
+	dup := key*
+	dup.Token = make([]byte, len(key.Token))
+	copy(dup.Token, key.Token)
+	return dup
 }
